cc1278f04d87d6d00404808da6c7569078812629
ptr-225 <soredemoii001396@gmail.com>
2025-02-03 01:34:00 +0900

    250203

--- src/main.tsx
+++ src/main.tsx
@@ -4,7 +4,7 @@ import { createRoot } from "react-dom/client";
 import { App } from "./App";
 import './index.css';
 
-const root = createRoot(document.getElementById("root") as Element);
+const root = createRoot( document.getElementById('root') as Element);
 
 root.render(
   <StrictMode>
